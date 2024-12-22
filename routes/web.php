3cb6d57eb00839b79599cd02ed14271561b10fe0
Usamah Abdul Aziz <ikmal.usamah@gmail.com>
2024-12-22 13:59:40 +0700

    akun user

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,12 @@ use App\Http\Controllers\CommentController;
 Route::get('/', [BlogController::class, 'index'])->name('home');
 
 // Rute resource untuk blogs
-Route::resource('blogs', BlogController::class);
-
+Route::resource('blogs', BlogController::class)->middleware('auth');
 
 // Route untuk menyimpan komentar pada blog
 Route::post('blogs/{blog}/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::resource('comments', CommentController::class)->except(['create', 'store']); // Untuk admin atau pengelolaan komentar.
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
